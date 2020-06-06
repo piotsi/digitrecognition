060d8a0433c53ad702e52b96e761cefb52b23b0b
Piotr Sikora <piotrsikoraaa@gmail.com>
2020-06-06 22:42:49 +0200

    Added couple of new functions

--- main.go
+++ main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	nn "digitrecognition/nn"
+	"fmt"
+	"math/rand"
 	"time"
+	// draw "github.com/piotsik/draw"
 )
 
 func main() {
-	nn.PNGtoCSV()
-	records := make(chan []string)
-	go nn.OpenMNIST(records, 10, "train")
-	nn.DrawDigit(records)
+	// nn.PNGtoCSV()
+	// records := make(chan []string)
+	// go nn.OpenMNIST(records, 10, "train")
+	// nn.DrawDigit(records)
+	// draw.Run()
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
 
-	time.Sleep(time.Millisecond)
+	a, _ := nn.CreateRandom(2, 1, 1)
+	b, _ := nn.CreateRandom(2, 1, 1)
+	fmt.Println("a: ", a)
+	fmt.Println("b: ", b)
+	_ = nn.ApplyFunc(b, nn.Sigmoid)
+	fmt.Println("b: ", b)
+	_ = nn.ScalarMul(b, 0.1)
+	fmt.Println("b: ", b)
+	_ = nn.ScalarAdd(b, 1.0)
+	fmt.Println("b: ", b)
+
+	fmt.Printf("Time taken: %s", time.Since(start))
+
+	// time.Sleep(time.Millisecond)
 }
--- nn/matrixmath.go
+++ nn/matrixmath.go
@@ -1,16 +1,64 @@
 package nn
 
-import "errors"
+import (
+	"errors"
+	"math"
+	"math/rand"
+)
+
+// Mat represents matrix
+type Mat [][]float64
+
+type fn func(float64) float64
 
 // These are errors
 var (
 	ErrEmptyMatrix          = errors.New("empty matrix")
-	ErrZeroLengthColsOrRows = errors.New("cannot create matrix with zero length columns or rows")
+	ErrZeroLengthColsOrRows = errors.New("zero length columns or row given")
 	ErrNumColNotEqualRows   = errors.New("number of columns in first matrix and number of rows in second matrix are not equal, cannot multiply")
+	ErrNotCorresponding     = errors.New("matrices are not the same sizes")
 )
 
-// MatSize takes matrix a, returns number of rows (m), columns (n) and error
-func MatSize(a [][]float64) (int, int, error) {
+// Sigmoid function
+func Sigmoid(x float64) float64 {
+	return 1 / (1 + math.Exp(-x))
+}
+
+// Create takes number of rows and columns, returns zero valued matrix and an error
+func Create(m, n int) (Mat, error) {
+	if m == 0 || n == 0 {
+		return nil, ErrZeroLengthColsOrRows
+	}
+	a := make(Mat, m)
+	for i := range a {
+		a[i] = make([]float64, n)
+	}
+
+	return a, nil
+}
+
+// CreateRandom takes number of rows and columns, returns random valued (uniform distribution) matrix and an error
+func CreateRandom(m, n int, v float64) (Mat, error) {
+	if m == 0 || n == 0 {
+		return nil, ErrZeroLengthColsOrRows
+	}
+	a := make(Mat, m)
+
+	min := -1 / math.Sqrt(v)
+	max := 1 / math.Sqrt(v)
+
+	for i := range a {
+		a[i] = make([]float64, n)
+		for j := 0; j < n; j++ {
+			a[i][j] = min + rand.Float64()*(max-min)
+		}
+	}
+
+	return a, nil
+}
+
+// Size takes matrix a, returns number of rows (m), columns (n) and error
+func Size(a Mat) (int, int, error) {
 	if a == nil {
 		return 0, 0, ErrEmptyMatrix
 	}
@@ -18,14 +66,66 @@ func MatSize(a [][]float64) (int, int, error) {
 	return len(a), len(a[0]), nil
 }
 
-// MatMul takes two matrices, a and b, returns product of their multiplication and an error
-func MatMul(a, b [][]float64) ([][]float64, error) {
-	// Get sizes of matrix a and b
-	n, m1, err := MatSize(a)
+// Add adds two matrices, returns new matrix and error
+func Add(a, b Mat) (Mat, error) {
+	m, n, err := Size(a)
 	if err != nil {
 		return nil, err
 	}
-	m2, p, err := MatSize(b)
+	m2, n2, err := Size(b)
+	if err != nil {
+		return nil, err
+	}
+	if m != m2 || n != n2 {
+		return nil, ErrNotCorresponding
+	}
+
+	c, err := Create(m, n)
+	if err != nil {
+		return nil, err
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			c[i][j] = a[i][j] + b[i][j]
+		}
+	}
+	return c, nil
+}
+
+// Subtract subtracts two matrices, returns new matrix and error
+func Subtract(a, b Mat) (Mat, error) {
+	m, n, err := Size(a)
+	if err != nil {
+		return nil, err
+	}
+	m2, n2, err := Size(b)
+	if err != nil {
+		return nil, err
+	}
+	if m != m2 || n != n2 {
+		return nil, ErrNotCorresponding
+	}
+
+	c, err := Create(m, n)
+	if err != nil {
+		return nil, err
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			c[i][j] = a[i][j] - b[i][j]
+		}
+	}
+	return c, nil
+}
+
+// Mul takes two matrices, a and b, returns product of their multiplication and an error
+func Mul(a, b Mat) (Mat, error) {
+	// Get sizes of matrix a and b
+	n, m1, err := Size(a)
+	if err != nil {
+		return nil, err
+	}
+	m2, p, err := Size(b)
 	if err != nil {
 		return nil, err
 	}
@@ -34,7 +134,7 @@ func MatMul(a, b [][]float64) ([][]float64, error) {
 		return nil, ErrNumColNotEqualRows
 	}
 
-	c, err := MatCreate(n, p)
+	c, err := Create(n, p)
 	if err != nil {
 		return nil, err
 	}
@@ -50,29 +150,44 @@ func MatMul(a, b [][]float64) ([][]float64, error) {
 	return c, nil
 }
 
-// MatScalMul takes matrix and a scalar float64, returns product of their multiplication and an error
-func MatScalMul(a [][]float64, c float64) ([][]float64, error) {
-	m, n, err := MatSize(a)
+// ScalarMul takes matrix and a scalar float64, returns error
+func ScalarMul(a Mat, c float64) error {
+	m, n, err := Size(a)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			a[i][j] = c * a[i][j]
 		}
 	}
-	return a, nil
+	return nil
 }
 
-// MatCreate takes number of rows and columns, returns zero valued matrix and an error
-func MatCreate(m, n int) ([][]float64, error) {
-	if m == 0 || n == 0 {
-		return nil, ErrZeroLengthColsOrRows
+// ScalarAdd takes matrix and a scalar float64, returns error
+func ScalarAdd(a Mat, c float64) error {
+	m, n, err := Size(a)
+	if err != nil {
+		return err
 	}
-	a := make([][]float64, m)
-	for i := range a {
-		a[i] = make([]float64, n)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			a[i][j] += c
+		}
 	}
+	return nil
+}
 
-	return a, nil
+// ApplyFunc applies function to a given matrix, returns error
+func ApplyFunc(a Mat, f fn) error {
+	m, n, err := Size(a)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			a[i][j] = f(a[i][j])
+		}
+	}
+	return nil
 }
